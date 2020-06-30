0a1d4d992496ac14b436492839c9e5fec6811fba
ahmadmhasann <ahmadmhasann@gmail.com>
2020-06-30 16:35:10 +0200

    first commit

---
+++ smart-home/main.c
@@ -0,0 +1,18 @@
+/*
+ * smart-home.c
+ *
+ * Created: 6/30/2020 16:29:58
+ * Author : Hassan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
